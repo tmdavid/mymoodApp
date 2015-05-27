a7db553fcd9fbd73e842ccffc2005cd076d75281
dtorrejon-ob <dtorrejon@oneboxtm.com>
2015-05-27 15:18:16 +0200

    first template?onview

--- mymoodApp/urls.py
+++ mymoodApp/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from mymood.views import *
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url('^yourmood/$', welcomepage),
+    url('^$',welcomepage),
+    url('^about/$',about),
 ]
