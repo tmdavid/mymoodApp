0951b9afa1b7cdedae5341aa5b0710eb4a233d88
David <torrejonmoya.david@gmail.com>
2016-11-07 15:15:03 +0100

    Update settings.py

--- mymoodApp/settings.py
+++ mymoodApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6@obe1tsh#&*%5vmj@%wt*vhv%*7cokbdkufi9xd_92-*byyx7'
+
 
 #APPEND_SLASH=False
 
